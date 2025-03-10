20d7b34de690b3da21480b8773e8b2ee84d170dc
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2025-03-10 21:20:15 +0000

    addEventListener

--- codereview/index.js
+++ codereview/index.js
@@ -1,5 +1,10 @@
-fetch("https://apis.scrimba.com/bored/api/activity")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("activity").innerText = data.activity
-    })
+document.getElementById("button").addEventListener("click", function() {
+    fetch("https://apis.scrimba.com/bored/api/activity")
+        .then(response => response.json())
+        .then(data => { 
+    document.getElementById("ideas").textContent = data.activity
+  })
+})
+
+ 
+
