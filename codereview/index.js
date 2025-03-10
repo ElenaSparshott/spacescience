f85ed346c0e9c57eb5ce5dfe0cabce0b9f0a4a01
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2025-03-10 11:23:49 +0000

    css

---
+++ codereview/index.js
@@ -0,0 +1,5 @@
+fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("activity").innerText = data.activity
+    })
