bec300d3cca60b57a41bbfa5ad0453d3cea35e06
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2025-03-11 09:37:07 +0000

    amination

--- codereview/index.js
+++ codereview/index.js
@@ -6,5 +6,21 @@ document.getElementById("button").addEventListener("click", function() {
   })
 })
 
+const spinning = [
+  { transform: "rotate(0) scale(0.5)" },
+  { transform: "rotate(360deg) scale(0.5)" },
+];
+
+const timing = {
+  duration: 2000,
+  iterations: 1,
+};
+
+const spinningRobot = document.getElementById("robot");
+
+document.addEventListener("click", () => {
+  spinningRobot.animate(spinning, timing);
+});
+
  
 
